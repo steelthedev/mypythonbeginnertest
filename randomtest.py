62585e95aba4e51900b62ff60fdf3dac526ad81b
Lordofmachine <akinwumikaliyanu@gmail.com>
2020-11-30 09:05:11 +0100

    first commit

---
+++ randomtest.py
@@ -0,0 +1,21 @@
+import random
+secretNumber=random.randint(1,20)
+
+print('I am thinking of a number between 1 and 20')
+
+for guessTaken in range(1,7):
+	print('Take a guess')
+	guess=int(input())
+	
+	if guess < secretNumber:
+		print('your guess is too low')
+	elif guess > secretNumber:
+		print('your guess is too high')
+	else:
+		break
+if guess == secretNumber:
+		print('Good job! you guessed right my number in ' + str(guessTaken) + ' guesses')	
+else:
+	  print('Nope i was thinking of ' + str(secretNumber))
+   
+	
